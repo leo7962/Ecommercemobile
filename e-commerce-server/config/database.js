db4957c885a9083302997c45277248cf671048c3
Leonardo Fabián Hernández Peña <ingenieroleonardo@outlook.com>
2021-05-25 14:58:27 -0500

    Chore: App with react native Initialized

---
+++ e-commerce-server/config/database.js
@@ -0,0 +1,20 @@
+module.exports = ({ env }) => ({
+  defaultConnection: 'default',
+  connections: {
+    default: {
+      connector: 'mongoose',
+      settings: {
+        host: env('DATABASE_HOST', 'strapi-commerce.nt3ea.mongodb.net'),
+        srv: env.bool('DATABASE_SRV', true),
+        port: env.int('DATABASE_PORT', 27017),
+        database: env('DATABASE_NAME', 'ecommerce'),
+        username: env('DATABASE_USERNAME', 'leo7962'),
+        password: env('DATABASE_PASSWORD', '3vHMhYzQufrUAfAD'),
+      },
+      options: {
+        authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
+        ssl: env.bool('DATABASE_SSL', true),
+      },
+    },
+  },
+});
---
+++ e-commerce-server/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '79c2bc97-e7cf-4cfd-99f0-634860af4bcf'
+};
