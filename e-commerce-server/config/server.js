bd48c4de0175112f2e8cb9738b57ea07ee95e4c6
Leonardo Fabián Hernández Peña <ingenieroleonardo@outlook.com>
2021-06-23 17:26:00 -0500

    Update Serves JS

--- e-commerce-server/config/server.js
+++ e-commerce-server/config/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  port: env.int('PORT', 8082),
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', '6600e8d4b47bcaf1cff9c184e47e0211'),
