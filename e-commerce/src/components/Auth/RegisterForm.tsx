e5c87bc31652092d051332f3fef4ee3a7e029b4b
Leonardo Fabián Hernández Peña <ingenieroleonardo@outlook.com>
2021-06-29 19:34:04 -0500

    Validation Form.

--- e-commerce/src/components/Auth/RegisterForm.tsx
+++ e-commerce/src/components/Auth/RegisterForm.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import {View} from "react-native";
 import {Button, TextInput} from "react-native-paper";
 import {useFormik} from "formik";
+import * as Yup from "yup";
 import {formStyles} from "../../styles";
 
 export default function RegisterForm(props: any) {
     const {changeForm} = props;
     const formik = useFormik({
         initialValues: initialValues(),
+        validationSchema: Yup.object(validationSchema()),
         onSubmit: (formData) => {
             console.log("Registro de usuario enviado");
             console.log(formData);
@@ -17,13 +19,18 @@ export default function RegisterForm(props: any) {
     return (
         <View>
             <TextInput label="Email" style={formStyles.input}
-                       onChangeText={(text) => formik.setFieldValue("email", text)}/>
+                       onChangeText={(text) => formik.setFieldValue("email", text)} value={formik.values.email}
+                       error={Boolean(formik.errors.email)}/>
             <TextInput label="Nombre de usuario" style={formStyles.input}
-                       onChangeText={(text) => formik.setFieldValue("username", text)}/>
+                       onChangeText={(text) => formik.setFieldValue("username", text)} value={formik.values.username}
+                       error={Boolean(formik.errors.username)}/>
             <TextInput label="Contraseña" style={formStyles.input} secureTextEntry
-                       onChangeText={(text) => formik.setFieldValue("password", text)}/>
+                       onChangeText={(text) => formik.setFieldValue("password", text)} value={formik.values.password}
+                       error={Boolean(formik.errors.password)}/>
             <TextInput label="Repetir contraseña" style={formStyles.input} secureTextEntry
-                       onChangeText={(text) => formik.setFieldValue("repeatPassword", text)}/>
+                       onChangeText={(text) => formik.setFieldValue("repeatPassword", text)}
+                       value={formik.values.repeatPassword}
+                       error={Boolean(formik.errors.repeatPassword)}/>
             <Button mode={"contained"} style={formStyles.btnSucces} onPress={formik.handleSubmit}>Registrarse</Button>
             <Button mode={"text"} style={formStyles.btnText} labelStyle={formStyles.btnTextLabel} onPress={changeForm}>Iniciar
                 sesión</Button>
@@ -37,5 +44,14 @@ function initialValues() {
         username: "",
         password: "",
         repeatPassword: ""
+    }
+}
+
+function validationSchema() {
+    return {
+        email: Yup.string().email().required(),
+        username: Yup.string().required(),
+        password: Yup.string().required(),
+        repeatPassword: Yup.string().required().oneOf([Yup.ref("password")]),
     }
 }
