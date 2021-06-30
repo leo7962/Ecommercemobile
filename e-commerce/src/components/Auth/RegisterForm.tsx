f5c8d780c35964ab75e0871a02051e19d4d342f8
Leonardo Fabián Hernández Peña <ingenieroleonardo@outlook.com>
2021-06-29 21:06:50 -0500

    RegisterForm.tsx for Strapi modified

--- e-commerce/src/components/Auth/RegisterForm.tsx
+++ e-commerce/src/components/Auth/RegisterForm.tsx
@@ -11,16 +11,12 @@ export default function RegisterForm(props: any) {
 
     const formik = useFormik({
         initialValues: initialValues(),
-        validationSchema: Yup.object().shape({
-            email: Yup.string().email().required(),
-            username: Yup.string().required(),
-            password: Yup.string().required('La contraseña es requerida').min(8, 'La contraseña no debe ser menor a 8 digitos'),
-            repeatPassword: Yup.string().required().oneOf([Yup.ref('password'), null], 'La contraseñas deben coincidir'),
-        }),
+        validationSchema: Yup.object().shape(validationSchema()),
         onSubmit: async (formData) => {
             try {
                 await registerApi(formData);
                 changeForm();
+                console.log("Ok");
             } catch (error) {
                 console.log(error);
             }
@@ -29,22 +25,20 @@ export default function RegisterForm(props: any) {
 
     return (
         <View>
-            <TextInput label={"Email"} style={formStyles.input}
+            <TextInput label="Email" style={formStyles.input}
                        onChangeText={(text) => formik.setFieldValue("email", text)} value={formik.values.email}
                        error={Boolean(formik.errors.email)}/>
-            <TextInput label={"Nombre de usuario"} style={formStyles.input}
+            <TextInput label="Nombre de usuario" style={formStyles.input}
                        onChangeText={(text) => formik.setFieldValue("username", text)} value={formik.values.username}
                        error={Boolean(formik.errors.username)}/>
-            <TextInput label={"Contraseña"} secureTextEntry style={formStyles.input}
+            <TextInput label="Contraseña" style={formStyles.input} secureTextEntry
                        onChangeText={(text) => formik.setFieldValue("password", text)} value={formik.values.password}
                        error={Boolean(formik.errors.password)}/>
-            <TextInput label={"Repetir Contraseña"} secureTextEntry style={formStyles.input}
+            <TextInput label="Repetir contraseña" style={formStyles.input} secureTextEntry
                        onChangeText={(text) => formik.setFieldValue("repeatPassword", text)}
                        value={formik.values.repeatPassword}
                        error={Boolean(formik.errors.repeatPassword)}/>
-            <Button mode={"contained"} style={formStyles.btnSuccess} onPress={formik.handleSubmit}>
-                Registrarse
-            </Button>
+            <Button mode={"contained"} style={formStyles.btnSuccess} onPress={formik.handleSubmit}>Registrarse</Button>
             <Button mode={"text"} style={formStyles.btnText} labelStyle={formStyles.btnTextLabel} onPress={changeForm}>Iniciar
                 sesión</Button>
         </View>
@@ -57,5 +51,14 @@ function initialValues() {
         username: "",
         password: "",
         repeatPassword: ""
+    }
+}
+
+function validationSchema() {
+    return {
+        email: Yup.string().email().required(),
+        username: Yup.string().required(),
+        password: Yup.string().required('La contraseña es requerida').min(8, 'La contraseña no debe ser menor a 8 digitos'),
+        repeatPassword: Yup.string().required().oneOf([Yup.ref('password'), null], 'La contraseñas deben coincidir'),
     }
 }
