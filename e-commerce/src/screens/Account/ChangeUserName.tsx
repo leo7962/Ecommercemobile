f1d0c2bec3a9a64f76f377c51514333766178453
leo7962 <ingenieroleonardo@outlook.com>
2021-09-19 14:23:29 -0500

    ChangeUserName.tsx not finished yet

---
+++ e-commerce/src/screens/Account/ChangeUserName.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import {Text, View} from "react-native";
+
+export default function ChangeUserName() {
+    return (
+        //TODO: Realizar el formulario de cambiar el usuario.
+        <View>
+            <Text>Change Username</Text>
+        </View>
+    );
+}
